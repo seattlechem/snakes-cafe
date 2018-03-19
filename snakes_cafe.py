46c613cddf4f7ae1957259ec1b17552c1ebc5354
Peter Kim <seattlechem@yahoo.com>
2018-03-19 16:46:56 -0700

    initial setup / created welcome screen / started menu

---
+++ snakes_cafe.py
@@ -0,0 +1,11 @@
+print('*' * 40)
+print(('*' * 2) + '     Welcome to the Snakes Cafe!    ' + ('*' * 2))
+print(('*' * 2) + ' ' * 36 + ('*' * 2))
+print(('*' * 2) + '  To quit at any time, type "quit"  ' + ('*' * 2))
+print('*' * 40)
+
+menu = ['Appetizers', 'Entrees', 'Desserts', 'Drinks']
+
+menu[0] = {'Wings': 0, 'Cookies': 0, 'Spring Rolls': 0}
+menu[1] = {'Salmon': 0, 'Steak': 0, 'Meat Tornado': 0, 'A Literal Garden': 0}
+
