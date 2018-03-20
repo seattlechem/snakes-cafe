8e07b0eea2713afbc2b1d5ce60449a8711205835
Peter Kim <seattlechem@yahoo.com>
2018-03-20 16:21:53 -0700

    subtotal function completed

--- snakes_cafe.py
+++ snakes_cafe.py
@@ -1,8 +1,18 @@
-menus = {'Appetizers': {'Wings': 0, 'Cookies': 0, 'Spring Rolls': 0},
-         'Entrees': {'Salmon': 0, 'Steak': 0, 'Meat Tornado': 0,
-                     'A Literal Garden': 0},
-         'Desserts': {'Ice Cream': 0, 'Cake': 0, 'Pie': 0},
-         'Drinks': {'Coffee': 0, 'Tea': 0, 'Blood of the Innocent': 0}}
+menus = {'Appetizers': {('Wings', 2.00): 0, ('Cookies', 15.00): 0,
+         ('Spring Rolls', 4.00): 0, ('Rings', 4.00): 0,
+         ('Crab Dip', 4.00): 0, ('Sliders', 4.00): 0},
+         'Entrees': {('Salmon', 5.00): 0, ('Steak', 6.00): 0,
+                     ('Meat Tornado', 4.00): 0, ('A Literal Garden', 3.00): 0,
+                     ('Pasta', 4.00): 0, ('Ribs', 4.00): 0},
+         'Desserts': {('Ice Cream', 500.0): 0, ('Cake', 30.00): 0,
+                      ('Pie', 3.00): 0, ('Pudding', 4.00): 0,
+                      ('Fruit', 4.00): 0, ('Sorbet', 4.00): 0},
+         'Drinks': {('Coffee', 4.00): 0, ('Tea', 3.00): 0,
+                    ('Blood of the Innocent', 6.00): 0, ('Pop', 4.00): 0,
+                    ('Wine', 4.00): 0, ('Beer', 4.00): 0},
+         'Sides': {('Frys', 4.00): 0, ('Salad', 3.00): 0,
+                   ('Bread', 6.00): 0, ('Slaw', 4.00): 0, ('Soup', 4.00): 0,
+                   ('Rice', 4.00): 0}}
 
 
 def menu_welcome():
@@ -17,38 +27,53 @@ def menu_items():
     for key, value in menus.items():
         print(key)
         print('-' * 10)
-        for item in value:
-            print(item)
-        print()    
+        for tuple_item in value:
+            print('{:>22}, {:>22}'.format(tuple_item[0],
+                  '$' + str(tuple_item[1])))
 
 
-menu_items()
-
+# menu_items()
 
 
 def ordering():
     condition = True
     print(('*' * 40) + '\n' + ('*' * 2) +
-                  '  What would you like to order?  ' + ('*' * 2) + '\n' +
-                  ('*' * 40) + '\n' + 'Enter quit any time to exit' )
-    while condition == True:
+          'What would you like to order?  ' + ('*' * 2) + '\n' +
+          ('*' * 40) + '\n' + 'Enter quit any time to exit')
+    while condition:
             order = input('>' + '\t')
             order = order.title()
             if order == 'Quit':
                 condition = False
                 break
             else:
-                    for _, order_count in menus.items():
-                        for item in order_count:
-                            if order == item:
-                                order_count[item] += 1
-                                print('** ' + str(order_count[item])+ ' order of ' + item + ' have been added to your meal **') 
-                
-               
-    
+                for _, value in menus.items():
+                    for tuple_item, count in value.items():                   
+                        if order == tuple_item[0]:
+                            value[tuple_item] += 1
+                            print('** ' + str(value[tuple_item]) +
+                                  ' order of ' + tuple_item[0] +
+                                  ' have been added to your meal **')
+                            sub_total()
+                            print('{} {}'.format('Your current total is now: ', '$' + str(sub_total())))
+                        elif order == 'Order'
+                            
 
-menu_welcome()
+
+def sub_total():
+    total = 0.0
+    for _, value in menus.items():
+        # print(key, value)
+        for tuple_item, count in value.items():
+            # print(tuple_item[1], count)
+            total += tuple_item[1] * count
+    return total
+
+# menu_welcome()
+
+
 menu_items()
 ordering()
+
 
 
