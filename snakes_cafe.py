35a67a3078970840aa96f979cf38440631b426bc
Peter Kim <seattlechem@yahoo.com>
2018-03-22 15:28:07 -0700

    adding_item_to_cart() completed

--- snakes_cafe.py
+++ snakes_cafe.py
@@ -2,30 +2,35 @@
 import csv
 import uuid
 
-menus = {'Appetizers': {('Wings', 2.00): 0, ('Cookies', 15.00): 0,
-         ('Spring Rolls', 4.00): 0, ('Rings', 4.00): 0,
-         ('Shrimp bites', 4.00): 0, ('Wontons', 4.00): 0,
-         ('Crab Dip', 4.00): 0, ('Sliders', 4.00): 0,
-         ('Clams', 8.00): 0},
-         'Entrees': {('Salmon', 5.00): 0, ('Steak', 6.00): 0,
-                     ('Meat Tornado', 4.00): 0, ('A Literal Garden', 3.00): 0,
-                     ('Pasta', 4.00): 0, ('Ribs', 4.00): 0,
-                     ('Cabbage Rolls', 8.00): 0, ('Pizza', 4.00): 0,
-                     ('Paella', 4.00): 0},
-         'Desserts': {('Ice Cream', 500.0): 0, ('Cake', 30.00): 0,
-                      ('Pie', 3.00): 0, ('Pudding', 4.00): 0,
-                      ('Fruit', 4.00): 0, ('Sorbet', 4.00): 0,
-                      ('Torte', 4.00): 0, ('Flan', 4.00): 0,
-                      ('Apple', 4.00): 0},
-         'Drinks': {('Coffee', 4.00): 0, ('Tea', 3.00): 0,
-                    ('Blood of the Innocent', 6.00): 0, ('Pop', 4.00): 0,
-                    ('Wine', 4.00): 0, ('Beer', 4.00): 0,
-                    ('Sake', 4.00): 0, ('Cocoa', 4.00): 0,
-                    ('Evian', 4.00): 0},
-         'Sides': {('Frys', 4.00): 0, ('Salad', 3.00): 0,
-                   ('Bread', 6.00): 0, ('Slaw', 4.00): 0, ('Soup', 4.00): 0,
-                   ('Rice', 4.00): 0, ('Spinach', 4.00): 0,
-                   ('Sauce', 4.00): 0, ('Crab dip', 4.00): 0}}
+menus = {'Appetizers': {('Wings', 2.00): 3, ('Cookies', 15.00): 3,
+         ('Spring Rolls', 4.00): 3, ('Rings', 4.00): 3,
+         ('Shrimp bites', 4.00): 3, ('Wontons', 4.00): 3,
+         ('Crab Dip', 4.00): 3, ('Sliders', 4.00): 3,
+         ('Clams', 8.00): 3},
+         'Entrees': {('Salmon', 5.00): 3, ('Steak', 6.00): 3,
+                     ('Meat Tornado', 4.00): 3, ('A Literal Garden', 3.00): 3,
+                     ('Pasta', 4.00): 3, ('Ribs', 4.00): 3,
+                     ('Cabbage Rolls', 8.00): 3, ('Pizza', 4.00): 3,
+                     ('Paella', 4.00): 3},
+         'Desserts': {('Ice Cream', 500.0): 3, ('Cake', 30.00): 3,
+                      ('Pie', 3.00): 3, ('Pudding', 4.00): 3,
+                      ('Fruit', 4.00): 3, ('Sorbet', 4.00): 3,
+                      ('Torte', 4.00): 3, ('Flan', 4.00): 3,
+                      ('Apple', 4.00): 3},
+         'Drinks': {('Coffee', 4.00): 3, ('Tea', 3.00): 3,
+                    ('Blood of the Innocent', 6.00): 3, ('Pop', 4.00): 3,
+                    ('Wine', 4.00): 3, ('Beer', 4.00): 3,
+                    ('Sake', 4.00): 3, ('Cocoa', 4.00): 3,
+                    ('Evian', 4.00): 3},
+         'Sides': {('Frys', 4.00): 3, ('Salad', 3.00): 3,
+                   ('Bread', 6.00): 3, ('Slaw', 4.00): 3, ('Soup', 4.00): 3,
+                   ('Rice', 4.00): 3, ('Spinach', 4.00): 3,
+                   ('Sauce', 4.00): 3, ('Crab dip', 4.00): 3}}
+
+cart = {}
+
+
+user_input = ''
 
 
 def menu_welcome():
@@ -72,46 +77,57 @@ def ordering():
         in that category.
 
     """
+    global user_input
     condition = True
-    print(('*' * 40) + '\n' + ('*' * 2) +
-          'What would you like to order?  ' + ('*' * 2) + '\n' +
-          ('*' * 40) + '\n' + 'Enter quit any time to exit')
+    order_string = 'What would you like to order?'
+    order_string2 = 'Please enter item name and quantity separated by a comma.'
+    order_string3 = 'Enter quit any time to exit.'
+    order_string4 = 'Enter remove if you want to remove an item.'
+    print('{} {} {} {}'.format(order_string, order_string2,
+                               order_string3, order_string4))
     while condition:
-            order = input('>' + '\t')
-            order = order.title()
-            if order.split(' ')[0] == 'Quit':
+            user_input = input('>' + '\t')
+            user_input = user_input.title()
+            if ',' in user_input:
+                user_input_item = user_input.split(',')[0]
+                user_input_qty = user_input.split(',')[1]
+            else:
+                user_input_item = user_input
+                user_input_qty = 1
+            if user_input_item == 'Quit':
                 condition = False
                 break
-            elif order.split(' ')[0] == 'Order':
+            elif user_input_item == 'Order':
                 print_receipt()
-            elif order.split(' ')[0] == 'Remove':
+            elif user_input_item == 'Remove':
                 for value in menus.values():
                     for tuple_item in value.keys():
-                        if order.split(' ')[1] == tuple_item[0]:
+                        if user_input.split(' ')[1] == tuple_item[0]:
                             value[tuple_item] -= 1
                 print_receipt()
-            elif order.split(' ')[0] == 'Menu':
+            elif user_input_item == 'Menu':
                 menu_items()
-            elif order.split(' ')[0] in menus.keys():
+            elif user_input_item in menus.keys():
                 for key, value in menus.items():
-                    if order.split(' ')[0] == key:
+                    if user_input_item == key:
                         for key in value.keys():
                             print(key[0])
             else:
-                for value in menus.values():
-                    for tuple_item in value.keys():
-                        if order == tuple_item[0]:
-                            # add the item with quantity into shopping cart located in global space
-                            # item price quantity_placed
-                            # after adding, print statement what and how many have been added into
-                            # print your current total
-                            value[tuple_item] += 1
-                            print('** ' + str(value[tuple_item]) +
-                                  ' order of ' + tuple_item[0] +
-                                  ' have been added to your meal **')
-                            sub_total()
-                            print('{} {}'.format('Your current total is now: ',
-                                  '$' + str(sub_total())))
+                adding_item_to_cart(user_input_item, user_input_qty)
+                print('** {} order of {} have been added'
+                      .format(user_input_qty, user_input_item))
+                print('Your current total is now: ${}'
+                      .format(str(sub_total())))
+
+
+# new function (test needed)
+def adding_item_to_cart(item_name, num_of_item=1):
+    for key, value in menus.items():
+        for tuple_item in value:
+            if item_name == tuple_item[0]:
+                cart[tuple_item[0]] = {tuple_item[1]:
+                                       num_of_item}
+    return cart
 
 
 def sub_total():
@@ -119,9 +135,9 @@ def sub_total():
     runs subtotal for receipt
     """
     total = 0.00
-    for value in menus.values():
-        for tuple_item, count in value.items():
-            total += tuple_item[1] * count
+    for value in cart.values():
+        for item_price, count in value.items():
+            total += item_price * count
     return total
 
 
@@ -167,7 +183,6 @@ def ask_optional_menu():
     If you don't, please type 'No'""")
     user_input = input('>' + '\t')
     answer = user_input.title()
-    # while True:
     if answer == 'Yes':
         ask_file_path()
     elif answer == 'No':
@@ -185,7 +200,6 @@ def ask_file_path():
         your_menu = csv.reader(menu_csv, delimiter=',')
         custom_menu = {}
         for row in your_menu:
-            # read out each row and generate menus
             generate_menu(custom_menu, row)
         menus = custom_menu
         menu_items()
@@ -201,9 +215,6 @@ def generate_menu(custom_menu, arr):
         custom_menu[arr[1]][(arr[0], price)] = qty
     else:
         custom_menu[arr[1]] = {(arr[0], price): qty}
-
-
-# calling functions
 
 
 if __name__ == '__main__':
