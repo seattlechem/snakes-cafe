bb5718e216ca51abd03aa3c4ea3ff2b3ff4d7a75
Peter Kim <seattlechem@yahoo.com>
2018-03-28 16:45:02 -0700

    test for menu_items() complete

--- test_snakes_cafe.py
+++ test_snakes_cafe.py
@@ -2,6 +2,7 @@ import snakes_cafe
 
 
 def test_menu_welcome():
+    ''' This tests the printout of welcome message '''
     assert snakes_cafe.menu_welcome() == """
             ****************************************
             **     Welcome to the Snakes Cafe!    **
@@ -12,14 +13,18 @@ def test_menu_welcome():
 
 
 def test_menu_items():
+    ''' This tests the printout of menu items'''
     assert snakes_cafe.menu_items() == """
 Appetizers----------
 Wings,               $2.00
 Cookies,              $15.00
 Spring Rolls,               $4.00
 Rings,               $4.00
+Shrimp bites,               $4.00
+Wontons,               $4.00
 Crab Dip,               $4.00
 Sliders,               $4.00
+Clams,               $8.00
 
 Entrees----------
 Salmon,               $5.00
@@ -28,6 +33,9 @@ Meat Tornado,               $4.00
 A Literal Garden,               $3.00
 Pasta,               $4.00
 Ribs ,               $4.00
+Cabbage Rolls,               $8.00
+Pizza,               $4.00
+Paella,               $4.00
 
 Desserts----------
 Ice Cream,             $500.00
@@ -36,6 +44,9 @@ Pie  ,               $3.00
 Pudding,               $4.00
 Fruit,               $4.00
 Sorbet,               $4.00
+Torte,               $4.00
+Flan ,               $4.00
+Apple,               $4.00
 
 Drinks----------
 Coffee,               $4.00
@@ -44,6 +55,9 @@ Blood of the Innocent,               $6.00
 Pop  ,               $4.00
 Wine ,               $4.00
 Beer ,               $4.00
+Sake ,               $4.00
+Cocoa,               $4.00
+Evian,               $4.00
 
 Sides----------
 Frys ,               $4.00
@@ -52,6 +66,9 @@ Bread,               $6.00
 Slaw ,               $4.00
 Soup ,               $4.00
 Rice ,               $4.00
+Spinach,               $4.00
+Sauce,               $4.00
+Crab dip,               $4.00
 """
 
 
